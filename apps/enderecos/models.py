9bb4745910fd380069c331de79c2288526e07f00
elyasma9 <elyas.santana@pagueon.com.br>
2020-08-05 17:44:05 -0300

    feat(endereços): add crud de endereços

---
+++ apps/enderecos/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from apps.usuarios.models import CustomUser
+
+
+class Endereco(models.Model):
+    user = models.ForeignKey(CustomUser,
+                             on_delete=models.CASCADE,
+                             related_name='enderecos')
+    logradouro = models.CharField(max_length=80)
+    bairro = models.CharField(max_length=50)
+    cep = models.CharField(max_length=9)
+    cidade = models.CharField(max_length=80)
+    estado = models.CharField(max_length=80)
+    numero = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.cidade
