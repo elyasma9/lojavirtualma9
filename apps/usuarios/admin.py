5e9c9465123bb01f93926b167a6a28e1a8a5c753
elyasma9 <elyas.santana@pagueon.com.br>
2020-08-05 17:47:36 -0300

    style(usuarios): update fieldset admin.py

--- apps/usuarios/admin.py
+++ apps/usuarios/admin.py
@@ -12,13 +12,14 @@ class CustomUserAdmin(UserAdmin):
     list_display = ('email', 'is_staff', 'is_active',)
     list_filter = ('email', 'is_staff', 'is_active',)
     fieldsets = (
-        (None, {'fields': ('email', 'password')}),
+        (None, {'fields': ('email', 'nome', 'sobrenome', 'cpf', 'rg', 'telefone',)}),
         ('Permissions', {'fields': ('is_staff', 'is_active')}),
     )
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active')}
+            'fields': ('nome', 'sobrenome', 'email', 'password1', 'password2', 'cpf',
+                       'rg','telefone','is_staff', 'is_active')}
          ),
     )
     search_fields = ('email',)
