c4facc3eba786e69b87a026c908bd0bc9161793f
elyasma9 <elyas.santana@pagueon.com.br>
2020-08-06 11:50:59 -0300

    ci: add urls das apps(pedido, produto)

--- loja/urls.py
+++ loja/urls.py
@@ -4,11 +4,15 @@ from django.urls import path
 from rest_framework import routers
 from apps.usuarios.api.viewsets import UsuariosViewSet
 from apps.enderecos.api.viewsets import EnderecosViewSet
+from apps.produtos.api.viewsets import ProdutosViewSet
+from apps.pedidos.api.viewsets import PedidosViewSet
 
 
 router = routers.DefaultRouter()
 router.register('usuarios', UsuariosViewSet)
 router.register('enderecos', EnderecosViewSet)
+router.register('produtos', ProdutosViewSet)
+router.register('pedidos', PedidosViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
