79a1687c67289367b05aef763513e6641352f214
elyasma9 <elyas.santana@pagueon.com.br>
2020-08-06 11:47:05 -0300

    refactor(endereco): add nested relation

--- apps/enderecos/api/serializers.py
+++ apps/enderecos/api/serializers.py
@@ -1,8 +1,12 @@
 from rest_framework.serializers import ModelSerializer
 from apps.enderecos.models import Endereco
+from apps.usuarios.api.serializers import UsuariosSerializer
 
 
 class EnderecossSerializer(ModelSerializer):
+
+    user = UsuariosSerializer()
+
     class Meta:
         model = Endereco
         fields = ('id', 'user', 'logradouro', 'bairro',
