6a78c5bc832af3043bb9ab53932821aaca6e50bf
elyasma9 <elyas.santana@pagueon.com.br>
2020-08-06 10:11:57 -0300

    feat(pedidos): add CRUD pedidos

---
+++ apps/pedidos/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from apps.usuarios.models import CustomUser
+from apps.enderecos.models import Endereco
+from apps.produtos.models import Produto
+from django.utils import timezone
+
+class Pedido(models.Model):
+
+    STATUS_CHOICES = (
+        ("P", "Pedido realizado"),
+        ("F", "Fazendo"),
+        ("E", "Saiu para entrega"),
+    )
+
+    user = models.ForeignKey(
+        CustomUser, on_delete=models.CASCADE, related_name='pedido')
+    endereco = models.ForeignKey(
+        Endereco, on_delete=models.SET_NULL, related_name='destino', null=True, blank=True)
+    data_pedido = models.DateTimeField(default=timezone.now)
+    status = models.CharField(
+        max_length=1, choices=STATUS_CHOICES, blank=False, null=False)
+    produtos = models.ManyToManyField(Produto, related_name='carrinho')
+
+    def __str__(self):
+        return '%s, esse é o seu carrinho.' % (self.user.get_full_name())
